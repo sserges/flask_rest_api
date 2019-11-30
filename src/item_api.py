3510293b4b9794c606d405e1171dffc14e28dc0b
sserges <agounkeserge@gmail.com>
2019-11-30 17:09:35 +0000

    the-itemlist-and-creating-items

--- src/item_api.py
+++ src/item_api.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 
@@ -17,14 +17,21 @@ class Item(Resource):
         return {'item': None}, 404
     
     def post(self, name):
+        data = request.get_json()
         item = {
             'name': name,
-            'price': 12.00
+            'price': data['price']
         }
         items.append(item)
         return item, 201
 
 
-api.add_resource(Item, '/item/<string:name>')
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
 
-app.run(port=5000)
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+
+app.run(port=5000, debug=True)
