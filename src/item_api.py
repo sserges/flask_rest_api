35f49e1b68c1e67ed161ae11bd3fe6d719496a5a
sserges <agounkeserge@gmail.com>
2019-12-05 17:56:17 +0000

    optimising-our-final-code-and-request-parsing

--- src/item_api.py
+++ src/item_api.py
@@ -15,15 +15,26 @@ jwt = JWT(app, authenticate, identity)
 items = []
 
 class Item(Resource):
+
+    parser = reqparse.RequestParser()
+    parser.add_argument(
+        'price',
+        type=float,
+        required=True,
+        help="This field cannot be left blank!"
+    )
+
     @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x['name'] == name, items), None)
         return {'item': item}, 200 if item else 404
     
-    def post(self, name):
+    def post(self, name): 
         if next(filter(lambda x: x['name'] == name, items), None):
             return {'message': f"An item with name {name} already exists."}, 400
-        data = request.get_json()
+        
+        data = Item.parser.parse_args()
+
         item = {
             'name': name,
             'price': data['price']
@@ -38,14 +49,7 @@ class Item(Resource):
     
 
     def put(self, name):
-        parser = reqparse.RequestParser()
-        parser.add_argument(
-            'price',
-            type=float,
-            required=True,
-            help="This field cannot be left blank!"
-        )
-        data = parser.parse_args()
+        data = Item.parser.parse_args()
         item = next(filter(lambda x: x['name'] == name, items), None)
         if item is None:
             item = {
