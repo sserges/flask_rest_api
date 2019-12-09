21bbd9c3ba86a597fae640d6c5c7ae12947881df
sserges <agounkeserge@gmail.com>
2019-12-09 18:08:48 +0000

    signing-up-and-writing-users-to-a-database

--- src/app.py
+++ src/app.py
@@ -1,82 +1,75 @@
-from flask import (
-    Flask, 
-    jsonify, 
-    request,
-    render_template
-)
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
+from user import UserRegister
 
 
 app = Flask(__name__)
+app.secret_key = 'v5INH8Pal09ZD6Omr11r2Jspr3KJOshr'
+api = Api(app)
 
-stores = [
-    {
-        'name': 'My Wonderful Store',
-        'items': [
-            {
-                'name': 'My Item',
-                'price': 15.99
-            }
-        ]
-    }
-]
-
-@app.route('/')
-def home():
-    return render_template('index.html')
+jwt = JWT(app, authenticate, identity)
 
 
-# POST /store data:{name}
-@app.route('/store', methods=['POST'])
-def create_store():
-    request_data = request.get_json()
-    new_store = {
-        'name': request_data['name'],
-        'items': []
-    }
-    stores.append(new_store)
-    return jsonify(new_store)
+items = []
 
+class Item(Resource):
 
-#GET /store/<string:name>
-@app.route('/store/<string:name>')
-def get_store(name):
-    for store in stores:
-        if store['name'] == name:
-            return jsonify(store)
+    parser = reqparse.RequestParser()
+    parser.add_argument(
+        'price',
+        type=float,
+        required=True,
+        help="This field cannot be left blank!"
+    )
+
+    @jwt_required()
+    def get(self, name):
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        return {'item': item}, 200 if item else 404
     
-    return jsonify({'message': 'store not found'})
+    def post(self, name): 
+        if next(filter(lambda x: x['name'] == name, items), None):
+            return {'message': f"An item with name {name} already exists."}, 400
+        
+        data = Item.parser.parse_args()
 
+        item = {
+            'name': name,
+            'price': data['price']
+        }
+        items.append(item)
+        return item, 201
+    
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {'message': 'Item deleted'}
+    
 
-# GET /store
-@app.route('/store')
-def get_stores():
-    return jsonify({"stores": stores})
-
-
-# POST /store/<string:name>/item {name; price}
-@app.route('/store/<string:name>/item', methods=['POST'])
-def create_item_in_store(name):
-    request_data = request.get_json()
-    for store in stores:
-        if store['name'] == name:
-            new_item = {
-                'name': request_data['name'],
-                'price': request_data['price']
+    def put(self, name):
+        data = Item.parser.parse_args()
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item is None:
+            item = {
+                'name': name,
+                'price': data['price']
             }
-            store['items'].append(new_item)
-            return jsonify(new_item)
-            
-    return jsonify({'message': 'store not found'})
+            items.append(item)
+        else:
+            item.update(data)
+        return item
 
 
-# GET /store/<string:name>/item 
-@app.route('/store/<string:name>/item')
-def get_items_in_store(name):
-    for store in stores:
-        if store['name'] == name:
-            return jsonify({'items': store['items']})
-    return jsonify({'message': 'store not found'})
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
 
 
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+api.add_resource(UserRegister, '/register')
 
-app.run(port=5000)
+app.run(port=5000, debug=True)
