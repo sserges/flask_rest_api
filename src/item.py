53367621b2be708a7d322a4a393b57b80b5ca6ce
sserges <agounkeserge@gmail.com>
2019-12-11 18:43:42 +0000

    refactoring-insertion-of-items

--- src/item.py
+++ src/item.py
@@ -13,12 +13,17 @@ class Item(Resource):
         help="This field cannot be left blank!"
     )
 
-    @jwt_required()
-    def get(self, name):
-        item = self.find_by_name(name)
-        if item:
-            return item
-        return {"message": "Item not found"}, 404
+
+    @classmethod
+    def insert(cls, item):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "INSERT INTO items VALUES (?, ?)"
+        cursor.execute(query, (item['name'], item['price']))
+
+        connection.commit()
+        connection.close()
     
     @classmethod
     def find_by_name(cls, name):
@@ -32,6 +37,13 @@ class Item(Resource):
 
         if row:
             return {"item": {'name': row[0], 'price': row[1]}}
+
+    @jwt_required()
+    def get(self, name):
+        item = self.find_by_name(name)
+        if item:
+            return item
+        return {"message": "Item not found"}, 404
     
     def post(self, name): 
         if self.find_by_name(name):
@@ -43,15 +55,11 @@ class Item(Resource):
             'name': name,
             'price': data['price']
         }
-        
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
 
-        query = "INSERT INTO items VALUES (?, ?)"
-        cursor.execute(query, (item['name'], item['price']))
-
-        connection.commit()
-        connection.close()
+        try:
+            self.insert(item)
+        except:
+            return {"message": "An error occurred inserting the item."}, 500 # Internal Server Error
 
         return item, 201
     
@@ -64,7 +72,7 @@ class Item(Resource):
 
         connection.commit()
         connection.close()
-        
+
         return {'message': 'Item deleted'}
     
 
