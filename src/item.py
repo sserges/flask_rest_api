e0f38d21c007e9fe1ede9c214d1d7a65382eda68
sserges <agounkeserge@gmail.com>
2019-12-11 17:56:47 +0000

    writing-our-item-resources-to-a-database

--- src/item.py
+++ src/item.py
@@ -15,6 +15,13 @@ class Item(Resource):
 
     @jwt_required()
     def get(self, name):
+        item = self.find_by_name(name)
+        if item:
+            return item
+        return {"message": "Item not found"}, 404
+    
+    @classmethod
+    def find_by_name(cls, name):
         connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
 
@@ -25,10 +32,9 @@ class Item(Resource):
 
         if row:
             return {"item": {'name': row[0], 'price': row[1]}}
-        return {"message": "Item not found"}, 404
     
     def post(self, name): 
-        if next(filter(lambda x: x['name'] == name, items), None):
+        if self.find_by_name(name):
             return {'message': f"An item with name {name} already exists."}, 400
         
         data = Item.parser.parse_args()
@@ -37,7 +43,16 @@ class Item(Resource):
             'name': name,
             'price': data['price']
         }
-        items.append(item)
+        
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "INSERT INTO items VALUES (?, ?)"
+        cursor.execute(query, (item['name'], item['price']))
+
+        connection.commit()
+        connection.close()
+
         return item, 201
     
     def delete(self, name):
