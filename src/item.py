bf661f4c8d3557f6425b8b05ccc64903617ffbe0
sserges <agounkeserge@gmail.com>
2019-12-13 17:26:10 +0000

    the-put-method-with-database-interaction

--- src/item.py
+++ src/item.py
@@ -24,7 +24,18 @@ class Item(Resource):
 
         connection.commit()
         connection.close()
-    
+
+    @classmethod
+    def update(cls, item):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "UPDATE items SET price=? WHERE name=?"
+        cursor.execute(query, (item['price'], item['name']))
+
+        connection.commit()
+        connection.close()
+
     @classmethod
     def find_by_name(cls, name):
         connection = sqlite3.connect('data.db')
@@ -78,16 +89,24 @@ class Item(Resource):
 
     def put(self, name):
         data = Item.parser.parse_args()
-        item = next(filter(lambda x: x['name'] == name, items), None)
+
+        item = self.find_by_name(name)
+        updated_item = {
+            'name': name,
+            'price': data['price']
+        }
+
         if item is None:
-            item = {
-                'name': name,
-                'price': data['price']
-            }
-            items.append(item)
+            try:
+                self.insert(updated_item)
+            except:
+                return {"message": "An error occurred inserting the item"}, 500
         else:
-            item.update(data)
-        return item
+            try:
+                self.update(updated_item)
+            except:
+                return {"message": "An error occurred updating the item"}, 500
+        return updated_item
 
 
 class ItemList(Resource):
