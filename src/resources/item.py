a2445cd712606347633c8dc735f637c70657a193
sserges <agounkeserge@gmail.com>
2019-12-18 19:30:15 +0000

    creating-user-and-item-models

--- src/resources/item.py
+++ src/resources/item.py
@@ -3,6 +3,8 @@ import sqlite3
 from flask_restful import Resource, reqparse
 from flask_jwt import jwt_required
 
+from models.item import ItemModel
+
 class Item(Resource):
 
     parser = reqparse.RequestParser()
@@ -13,66 +15,27 @@ class Item(Resource):
         help="This field cannot be left blank!"
     )
 
-
-    @classmethod
-    def insert(cls, item):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "INSERT INTO items VALUES (?, ?)"
-        cursor.execute(query, (item['name'], item['price']))
-
-        connection.commit()
-        connection.close()
-
-    @classmethod
-    def update(cls, item):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "UPDATE items SET price=? WHERE name=?"
-        cursor.execute(query, (item['price'], item['name']))
-
-        connection.commit()
-        connection.close()
-
-    @classmethod
-    def find_by_name(cls, name):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM items WHERE name=?"
-        result = cursor.execute(query, (name,))
-        row = result.fetchone()
-        connection.close()
-
-        if row:
-            return {"item": {'name': row[0], 'price': row[1]}}
-
     @jwt_required()
     def get(self, name):
-        item = self.find_by_name(name)
+        item = ItemModel.find_by_name(name)
         if item:
-            return item
+            return item.json()
         return {"message": "Item not found"}, 404
     
     def post(self, name): 
-        if self.find_by_name(name):
+        if ItemModel.find_by_name(name):
             return {'message': f"An item with name {name} already exists."}, 400
         
         data = Item.parser.parse_args()
 
-        item = {
-            'name': name,
-            'price': data['price']
-        }
+        item = ItemModel(name, data['price'])
 
         try:
-            self.insert(item)
+            item.insert()
         except:
             return {"message": "An error occurred inserting the item."}, 500 # Internal Server Error
 
-        return item, 201
+        return item.json(), 201
     
     def delete(self, name):
         connection = sqlite3.connect('data.db')
@@ -90,23 +53,20 @@ class Item(Resource):
     def put(self, name):
         data = Item.parser.parse_args()
 
-        item = self.find_by_name(name)
-        updated_item = {
-            'name': name,
-            'price': data['price']
-        }
+        item = ItemModel.find_by_name(name)
+        updated_item = ItemModel(name, data['price'])
 
         if item is None:
             try:
-                self.insert(updated_item)
+                updated_item.insert()
             except:
                 return {"message": "An error occurred inserting the item"}, 500
         else:
             try:
-                self.update(updated_item)
+                updated_item.update()
             except:
                 return {"message": "An error occurred updating the item"}, 500
-        return updated_item
+        return updated_item.json()
 
 
 class ItemList(Resource):
@@ -125,4 +85,3 @@ class ItemList(Resource):
         connection.close()
 
         return {'items': items}
-    
--- src/security.py
+++ src/security.py
@@ -1,6 +1,6 @@
 from werkzeug.security import safe_str_cmp
 
-from resources.user import User
+from models.user import UserModel
 
 # users = [
 #     User(1, 'bob', 'asdf')
@@ -16,12 +16,12 @@ from resources.user import User
 
 
 def authenticate(username, password):
-    user = User.find_by_username(username)
+    user = UserModel.find_by_username(username)
     if user and safe_str_cmp(user.password, password):
         return user
 
 
 def identity(payload):
     user_id = payload['identity']
-    return User.find_by_id(user_id)
+    return UserModel.find_by_id(user_id)
 
